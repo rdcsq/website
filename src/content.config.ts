db7928bc945051d7f599f284cf90c55881944d79
rdcsq <git@rdcsq.com>
2024-12-31 12:40:31 -0700

    add md to content loader

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,7 +3,7 @@ import { defineCollection, z } from 'astro:content';
 
 export const collections = {
   blog: defineCollection({
-    loader: glob({ pattern: '**/*.mdx', base: './blog' }),
+    loader: glob({ pattern: '**/*.(md|mdx)', base: './blog' }),
     schema: ({ image }) =>
       z.object({
         title: z.string(),
