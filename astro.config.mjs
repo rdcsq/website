a43f551f050132dcda2aa298ee5d86df180a3c20
rdcsq <git@rdcsq.com>
2025-12-30 20:14:47 -0700

    use pnpm, upgrade astro and remove adapter deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,12 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
-import node from '@astrojs/node';
 
 export default defineConfig({
-    output: 'server',
+    output: 'static',
     vite: {
         plugins: [tailwindcss()],
     },
     integrations: [mdx()],
-    adapter: node({
-        mode: 'standalone',
-    }),
-    env: {
-        schema: {
-            LASTFM_API_KEY: envField.string({
-                context: 'server',
-                access: 'secret'
-            }),
-            LASTFM_USER: envField.string({
-                context: 'server',
-                access: 'secret'
-            }),
-        },
-    },
 });
